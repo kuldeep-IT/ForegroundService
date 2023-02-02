18bba14ca820920fd3881131dd6f9613242b2362
kuldeep-IT <rathod99244@gmail.com>
2023-02-02 14:28:50 +0530

    - Detected New App on install and notified it user by Notification.

--- app/src/main/java/com/dada/mediumservice/MainActivity.java
+++ app/src/main/java/com/dada/mediumservice/MainActivity.java
@@ -1,21 +1,29 @@
 package com.dada.mediumservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import com.dada.mediumservice.app.MyApp;
 import com.dada.mediumservice.service.AutoStartService;
+import com.dada.mediumservice.service.StartUpBootReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
     /*
      * https://medium.com/swlh/all-about-auto-start-foreground-service-in-android-8bba7569123e
      * */
+
+    private StartUpBootReceiver startUpBootReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("IS_SERVICE_RUNNING", "onCreate: "+ MyApp.isServiceRunningInForeground(MainActivity.this, AutoStartService.class));
         /* MyApp.isServiceRunningInForeground(MainActivity.this, AutoStartService.class);*/
+
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_PACKAGE_ADDED);
+        filter.addDataScheme("package");
+        registerReceiver(mReceiver, filter);
     }
 
     public void startService(View v) {
@@ -38,5 +51,35 @@ public class MainActivity extends AppCompatActivity {
         Intent serviceIntent = new Intent(this, AutoStartService.class);
         stopService(serviceIntent);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_PACKAGE_ADDED);
+        filter.addDataScheme("package");
+        startUpBootReceiver = new StartUpBootReceiver();
+        registerReceiver(startUpBootReceiver, filter);
+
+    }
+
+    //Extra added
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
+                String packageName = intent.getData().getSchemeSpecificPart();
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("New App Installed")
+                        .setContentText(packageName + " was installed.");
+                NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0, builder.build());
+            }
+        }
+
+
+    };
 
 }
--- app/src/main/java/com/dada/mediumservice/service/StartUpBootReceiver.java
+++ app/src/main/java/com/dada/mediumservice/service/StartUpBootReceiver.java
@@ -1,12 +1,24 @@
 package com.dada.mediumservice.service;
 
+import static com.dada.mediumservice.app.MyApp.CHANNEL_ID;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.LongDef;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.dada.mediumservice.R;
+import com.dada.mediumservice.app.MyApp;
 
 public class StartUpBootReceiver  extends BroadcastReceiver {
    /* @Override
@@ -17,16 +29,75 @@ public class StartUpBootReceiver  extends BroadcastReceiver {
         }
     }*/
 
+    // https://stackoverflow.com/questions/45462666/notificationcompat-builder-deprecated-in-android-o
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_PACKAGE_ADDED.equals(intent.getAction())) {
+        /*if (Intent.ACTION_PACKAGE_ADDED.equals(intent.getAction())) {
             String packageName = intent.getData().getSchemeSpecificPart();
             // Perform operations when a new package is installed
             Toast.makeText(context, "Package Added: " + packageName, Toast.LENGTH_SHORT).show();
 
             Log.d("HAlo", "Package Added: " + packageName);
+        }*/
+
+        Log.d("NEW_APP_DETECTED", "Intent Action: " + intent.getAction());
+        Uri data = intent.getData();
+        Log.d("NEW_APP_DETECTED", " Intent DATA: " + data);
+
+
+        //second added
+        if (intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
+            String packageName = intent.getData().getSchemeSpecificPart();
+
+            showNotifcation(context,"New App Detected","Here is your new app: "+packageName);
+            Toast.makeText(context, "Package Added: " + packageName, Toast.LENGTH_SHORT).show();
+            Log.d("NEW_APP_DETECTED", "Here is your new app: "+packageName);
+            Notification.Builder builder = new Notification.Builder(context)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("New App Installed")
+                    .setContentText(packageName + " was installed.");
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, builder.build());
         }
     }
+
+    private void showNotifcation(Context context, String title, String body) {
+        //Este método muestra notificaciones compatibles con Android Api Level < 26 o >=26
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //Mostrar notificacion en Android Api level >=26
+//            final String CHANNEL_ID = "HEADS_UP_NOTIFICATIONS";
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    MyApp.CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            context.getSystemService(NotificationManager.class).createNotificationChannel(channel);
+            Notification.Builder notification = new Notification.Builder(context, CHANNEL_ID)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setAutoCancel(true);
+            NotificationManagerCompat.from(context).notify(1, notification.build());
+
+        }else{
+            //Mostrar notificación para Android Api Level Menor a 26
+            String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setAutoCancel(true);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+
+        }
+
+    }
+
+
 
 }
